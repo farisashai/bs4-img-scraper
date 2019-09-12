7ecfbebb3506ae697476dfaf0e214287df548c41
Faris Ashai <farisashai@gmail.com>
2019-09-12 15:12:03 -0700

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -8,7 +8,7 @@ url = 'https://www.google.com/search?tbm=isch&q={0}'.format(query)
 response = requests.get(url)
 content = BeautifulSoup(response.content,'html.parser')
 
-for img,num in zip(content.findAll('img'),range(1,100)):
+for num,img in enumerate(content.findAll('img')):
     string = img['src'];
     picture = requests.get(string)
     Image.open(BytesIO(picture.content)).convert('RGB').save('results/{0}.jpg'.format(num))
